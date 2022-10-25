62efdb18668cda2cd19fcd3ac9031a14ee842ad6
Ensar KURT <37beykozlu37@gmail.com>
2022-10-25 23:47:35 +0300

    added description

--- src/content/Management/SendMoney/SendMoneyContainer.tsx
+++ src/content/Management/SendMoney/SendMoneyContainer.tsx
@@ -1,185 +1,181 @@
 import LoadingButton from "@mui/lab/LoadingButton";
-import {Box, Card, CardContent, CardHeader, Divider, Grid, MenuItem} from "@mui/material";
-import {useEffect, useState} from "react";
-import {FormProvider, SubmitHandler, useForm} from "react-hook-form";
+import { Box, Card, CardContent, CardHeader, Divider, Grid, MenuItem } from "@mui/material";
+import { useEffect, useState } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 
 import FormInput from "@/components/FormInput/FormInput";
 import SelectInput from "@/components/SelectInput/SelectInput";
-import {trpc} from "@/utils/trpc";
-import {BankAccount, BankAccountType, CurrencyType} from "@prisma/client";
-import {TRPCClientError} from "@trpc/client";
-import {useRouter} from "next/router";
+import { trpc } from "@/utils/trpc";
+import { BankAccount, BankAccountType, CurrencyType } from "@prisma/client";
+import { TRPCClientError } from "@trpc/client";
+import { useRouter } from "next/router";
 import toast from "react-hot-toast";
 
 type Inputs = {
-    accountId: string;
-    amount: number;
-    toIban: string;
-    name: string;
+  accountId: string;
+  amount: number;
+  toIban: string;
+  name: string;
 };
 
 function sendMoneyContainer() {
-    const router = useRouter();
-    const [accounts, setAccounts] = useState<BankAccount[]>([]);
-    const [maxAmount, setMaxAmount] = useState<number>(0);
-    const data = trpc.useQuery(["account.list"]);
-    useEffect(() => {
-        if (data.status == "success" && data.isSuccess) {
-            if (data.data.accounts.length <= 0) {
-                toast.error("Para gönderebilmek için öncelikle bir banka hesabı açmalısınız.");
-                setTimeout(() => {
-                    router.push("/management/accounts");
-                }, 3000);
-                return;
-            }
-            const _accounts = data.data.accounts
-                .filter((account) => account.deletedAt == null)
-                .filter((account) => account.type == BankAccountType.CURRENT)
-                .filter((account) => account.currency != CurrencyType.GLD);
+  const router = useRouter();
+  const [accounts, setAccounts] = useState<BankAccount[]>([]);
+  const [maxAmount, setMaxAmount] = useState<number>(0);
+  const data = trpc.useQuery(["account.list"]);
+  useEffect(() => {
+    if (data.status == "success" && data.isSuccess) {
+      if (data.data.accounts.length <= 0) {
+        toast.error("Para gönderebilmek için öncelikle bir banka hesabı açmalısınız.");
+        setTimeout(() => {
+          router.push("/management/accounts");
+        }, 3000);
+        return;
+      }
+      const _accounts = data.data.accounts
+        .filter((account) => account.deletedAt == null)
+        .filter((account) => account.type == BankAccountType.CURRENT)
+        .filter((account) => account.currency != CurrencyType.GLD);
 
-            setAccounts(_accounts);
+      setAccounts(_accounts);
 
-            if (_accounts.length <= 0) {
-                toast.error("Para gönderebilmek cari ve açık bir hesabınız bulunamadı!");
-                setTimeout(() => {
-                    router.push("/management/accounts");
-                }, 3000);
-            }
-        }
-    }, [data.isLoading]);
+      if (_accounts.length <= 0) {
+        toast.error("Para gönderebilmek cari ve açık bir hesabınız bulunamadı!");
+        setTimeout(() => {
+          router.push("/management/accounts");
+        }, 3000);
+      }
+    }
+  }, [data.isLoading]);
 
-    const currencies = [
-        {
-            value: CurrencyType.USD,
-            label: "$",
-        },
-        {
-            value: CurrencyType.EUR,
-            label: "€",
-        },
-        {
-            value: CurrencyType.TRY,
-            label: "₺",
-        },
-    ];
+  const currencies = [
+    {
+      value: CurrencyType.USD,
+      label: "$",
+    },
+    {
+      value: CurrencyType.EUR,
+      label: "€",
+    },
+    {
+      value: CurrencyType.TRY,
+      label: "₺",
+    },
+  ];
 
-    const getCurrencyLabel = (currency: CurrencyType) => {
-        return currencies.filter((currencyItem) => currencyItem.value == currency)[0].label;
-    };
+  const getCurrencyLabel = (currency: CurrencyType) => {
+    return currencies.filter((currencyItem) => currencyItem.value == currency)[0].label;
+  };
 
-    const methods = useForm<Inputs>();
-    const transactionMutation = trpc.useMutation("transaction.create");
-    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+  const methods = useForm<Inputs>();
+  const transactionMutation = trpc.useMutation("transaction.create");
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      data.amount = parseFloat(data.amount.toString());
+      const response = await transactionMutation.mutateAsync(data);
+      toast.success(response.message);
+      setTimeout(() => {
+        router.push("/management/accounts");
+      }, 3000);
+    } catch (error: Error | any) {
+      if (error instanceof TRPCClientError) {
         try {
-            data.amount = parseFloat(data.amount.toString());
-            const response = await transactionMutation.mutateAsync(data);
-            toast.success(response.message);
-            setTimeout(() => {
-                router.push("/management/accounts");
-            }, 3000);
-        } catch (error: Error | any) {
-            if (error instanceof TRPCClientError) {
-                try {
-                    toast.error(JSON.parse(error.message)[0].message);
-                } catch {
-                    toast.error(error.message);
-                }
-            }
+          toast.error(JSON.parse(error.message)[0].message);
+        } catch {
+          toast.error(error.message);
         }
-    };
+      }
+    }
+  };
 
-    return (
-        <Grid item xs={12}>
-            <Card>
-                <CardHeader title="Input Fields"/>
-                <Divider/>
-                <CardContent>
-                    {data.isLoading && (
-                        <LoadingButton loading={true} variant="contained">
-                            Loading
-                        </LoadingButton>
-                    )}
-                    {accounts.length > 0 && (
-                        <FormProvider {...methods}>
-                            <Grid item container justifyContent="center" rowSpacing={5}>
-                                <Grid item xs={12} sm={6}>
-                                    <Box
-                                        display="flex"
-                                        flexDirection="column"
-                                        component="form"
-                                        noValidate
-                                        autoComplete="off"
-                                        onSubmit={methods.handleSubmit(onSubmit)}
-                                    >
-                                        <SelectInput
-                                            label={"Hesap Seçin"}
-                                            name={"accountId"}
-                                            mt={2}
-                                            onChanged={(val) => {
-                                                const account = accounts.filter((account) => account.id == val)[0];
-                                                setMaxAmount(parseFloat(account.balance.toString()));
-                                                console.log(account.balance);
-                                            }}
-                                            required
-                                        >
-                                            {accounts.map((option) => (
-                                                <MenuItem key={option.id} value={option.id}>
-                                                    {`Cari Hesap - ${option.currency} Hesabı | ${option.balance} ${getCurrencyLabel(
-                                                        option.currency
-                                                    )}`}
-                                                </MenuItem>
-                                            ))}
-                                        </SelectInput>
+  return (
+    <Grid item xs={12}>
+      <Card>
+        <CardHeader title="Input Fields" />
+        <Divider />
+        <CardContent>
+          {data.isLoading && (
+            <LoadingButton loading={true} variant="contained">
+              Loading
+            </LoadingButton>
+          )}
+          {accounts.length > 0 && (
+            <FormProvider {...methods}>
+              <Grid item container justifyContent="center" rowSpacing={5}>
+                <Grid item xs={12} sm={6}>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={methods.handleSubmit(onSubmit)}
+                  >
+                    <SelectInput
+                      label={"Hesap Seçin"}
+                      name={"accountId"}
+                      mt={2}
+                      onChanged={(val) => {
+                        const account = accounts.filter((account) => account.id == val)[0];
+                        setMaxAmount(parseFloat(account.balance.toString()));
+                        console.log(account.balance);
+                      }}
+                      required
+                    >
+                      {accounts.map((option) => (
+                        <MenuItem key={option.id} value={option.id}>
+                          {`Cari Hesap - ${option.currency} Hesabı | ${option.balance} ${getCurrencyLabel(
+                            option.currency
+                          )}`}
+                        </MenuItem>
+                      ))}
+                    </SelectInput>
 
-                                        <FormInput
-                                            name={"toIban"}
-                                            label={"Gönderilecek IBAN"}
-                                            type={"text"}
-                                            defaultValue={"TR"}
-                                            minLength={26}
-                                            maxLength={26}
-                                            required
-                                        />
+                    <FormInput
+                      name={"toIban"}
+                      label={"Gönderilecek IBAN"}
+                      type={"text"}
+                      defaultValue={"TR"}
+                      minLength={26}
+                      maxLength={26}
+                      required
+                    />
 
-                                        <FormInput
-                                            name={"name"}
-                                            label={"Hesap Sahibinin Adı"}
-                                            type={"text"}
-                                            minLength={1}
-                                            maxLength={255}
-                                            required
-                                        />
+                    <FormInput
+                      name={"name"}
+                      label={"Hesap Sahibinin Adı"}
+                      type={"text"}
+                      minLength={1}
+                      maxLength={255}
+                      required
+                    />
 
-                                        <FormInput
-                                            name={"amount"}
-                                            label={"Tutar"}
-                                            type={"number"}
-                                            max={maxAmount}
-                                            required
-                                        />
+                    <FormInput name={"amount"} label={"Tutar"} type={"number"} max={maxAmount} required />
 
-                                        <LoadingButton
-                                            loading={methods.formState.isSubmitting}
-                                            type="submit"
-                                            variant="contained"
-                                            sx={{
-                                                py: "0.8rem",
-                                                mt: 2,
-                                                width: "80%",
-                                                marginInline: "auto",
-                                            }}
-                                        >
-                                            Gönder
-                                        </LoadingButton>
-                                    </Box>
-                                </Grid>
-                            </Grid>
-                        </FormProvider>
-                    )}
-                </CardContent>
-            </Card>
-        </Grid>
-    );
+                    <FormInput name={"description"} label={"Açıklama"} type={"text"} maxLength={255} />
+
+                    <LoadingButton
+                      loading={methods.formState.isSubmitting}
+                      type="submit"
+                      variant="contained"
+                      sx={{
+                        py: "0.8rem",
+                        mt: 2,
+                        width: "80%",
+                        marginInline: "auto",
+                      }}
+                    >
+                      Gönder
+                    </LoadingButton>
+                  </Box>
+                </Grid>
+              </Grid>
+            </FormProvider>
+          )}
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
 export default sendMoneyContainer;
